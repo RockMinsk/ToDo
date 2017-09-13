9a2b84ea0ec63bbf2774cb2ac743db415112f864
Kiryl <shytsikau.kiryl@gmail.com>
2017-09-13 16:59:00 +0300

    module jsonfile + todos.js update

--- app/todos.js
+++ app/todos.js
@@ -1,9 +1,22 @@
-let todos = ["test", "test 3", "test 4"];
+const jsonfile = require('jsonfile');
+const file = './todosArr.json';
+const fs = require('fs');
+
+/*let todos = jsonfile.readFile(file, function(err, obj) {
+  	err ? console.log("File is not found") : obj.todo;
+});*/
+
+let todos = jsonfile.readFileSync(file).todo;
+
+//let y = fs.readFile('./todosArr.json', (err, data) => err ? console.log ("BAD") : console.log(JSON.parse(data)));
+
+//todos = require('./todosArr.json').todo;
+console.log(todos);
 
 const myModule = {
 	getTodos: () => todos,
 	addTodo: (todo) => todos.push,
-	deleteTodo: (todo) => todos.filter((el, index) => index != todos.indexOf(todo));
+	deleteTodo: (todo) => todos.filter((el, index) => index != todos.indexOf(todo))
 };
 
 module.exports = myModule;
