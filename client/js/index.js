06c73a0ecf433045edf9caebe6e34d3a244e049c
Kiryl <shytsikau.kiryl@gmail.com>
2017-09-15 19:04:15 +0300

    small updates in the index.js

--- client/js/index.js
+++ client/js/index.js
@@ -4,6 +4,29 @@ $.ajax({
 		type:'GET',
 		url: '/api/todos',
 		success: todos => {
-			$.each(todos, (i,todo) => $todos.append('<li>' + todo.title + '</li>'));
-		}
-	});
+			$.each(todos, (i,todo) => {
+				let li = document.createElement("li");
+    			li.innerHTML = '<div class="view"><input class="toggle" type="checkbox"><label>' + 
+					todo.title + '</label><button class="destroy"></button></div>' + 
+					'<input class="edit" value="' + todo.title + '">';
+				$todos.append(li);
+				});
+			}
+		});
+
+$('#new-todo').keydown(function (e) {
+ 				let key = e.which;
+ 				if(key == 13)  {
+   					alert("uhuhuhuh");
+   				}
+   			});
+
+            //TOOGLE
+$(".toggle").click(function(){
+    			alert("cccccc");
+    			});
+
+            //DELETE
+$(".destroy").click(function(){
+    			alert("bbbbbb");
+    			});
